eb27f8ed31b5d4767a7bccf68c1267184463005c
JustAnOtherCoder13 <justanothercoder13@gmail.com>
2020-07-10 17:20:14 +0200

    maps implemented with static positions

--- app/src/main/java/com/picone/go4lunch/presentation/ui/MapsFragment.java
+++ app/src/main/java/com/picone/go4lunch/presentation/ui/MapsFragment.java
@@ -1,24 +1,43 @@
 package com.picone.go4lunch.presentation.ui;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.picone.go4lunch.databinding.FragmentMapsBinding;
 
+import java.util.Objects;
 
-public class MapsFragment extends Fragment {
+
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentMapsBinding mBinding;
+    private GoogleMap mMap;
+    private boolean mLocationPermissionGranted;
+    private final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 13700;
+    private final LatLng DEFAULT_LOCATION = new LatLng(-33.852, 151.211);
+    private final int DEFAULT_ZOOM = 12;
 
     public static MapsFragment newInstance() {
         return new MapsFragment();
     }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,6 +48,67 @@ public class MapsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = FragmentMapsBinding.inflate(inflater, container, false);
+        mBinding.mapView.onCreate(savedInstanceState);
+        mBinding.mapView.getMapAsync(this);
+        mBinding.mapView.onResume();
+        try {
+            MapsInitializer.initialize(requireActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        updateLocationUI();
+        googleMap.addMarker(new MarkerOptions().position(DEFAULT_LOCATION).title("Your Position"));
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(DEFAULT_LOCATION).zoom(DEFAULT_ZOOM).build();
+        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+            }
+        }
+        updateLocationUI();
+    }
+    private void updateLocationUI() {
+        if (mMap == null) {
+            return;
+        }
+        try {
+            if (mLocationPermissionGranted) {
+                mMap.setMyLocationEnabled(true);
+                mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                mMap.setMyLocationEnabled(false);
+                mMap.getUiSettings().setMyLocationButtonEnabled(false);
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", Objects.requireNonNull(e.getMessage()));
+        }
+    }
+    private void getLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this.requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mLocationPermissionGranted = true;
+        } else {
+            ActivityCompat.requestPermissions(this.requireActivity(),
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
     }
 }
